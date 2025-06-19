22f2bc5143f021607c64f260da04c2acf9e735da
Filip Vukovinski <filip.vukovinski@net.hr>
2025-06-19 11:54:13 +0200

    Added CreatedAt field to Quiz model

--- QuizMaker.Models/Migrations/20250619095320_Quiz_CreatedAt.Designer.cs
+++ QuizMaker.Models/Migrations/20250619095320_Quiz_CreatedAt.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using QuizMaker.Models;
@@ -11,9 +12,11 @@ using QuizMaker.Models;
 namespace QuizMaker.Models.Migrations
 {
     [DbContext(typeof(QuizMakerDbContext))]
-    partial class QuizMakerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250619095320_Quiz_CreatedAt")]
+    partial class Quiz_CreatedAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- QuizMaker.Models/Models/Quiz.cs
+++ QuizMaker.Models/Models/Quiz.cs
@@ -3,7 +3,8 @@
     public class Quiz
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public required string Title { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
 
         public ICollection<QuizQuestion> QuizQuestions { get; set; }
     }
